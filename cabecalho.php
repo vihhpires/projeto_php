fb65fe04af081ccc0a11ff8ae322b20adbdad121
vihhpires <vitoriapquenatalento@gmail.com>
2026-08-06 11:49:09 -0300

    aula 02 conexão banco de dados

---
+++ cabecalho.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projeto PHP - GRUD</title>
+</head>
+
+<body>
+    <header>
+        <h1>Sistema de Produtos</h1>
+        <nav>
+            <a href="index.php">Início</a>
+            <a href="produtos/listar.php">Produtos</a>
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
---
+++ rodape.php
@@ -0,0 +1,8 @@
+
+    <footer>
+        <p>Sistema de Produtos - Todos os direitos reservados</p>
+    </footer>
+
+</body>
+
+</html>
